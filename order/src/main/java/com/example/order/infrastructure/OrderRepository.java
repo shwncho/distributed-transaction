8d701d6a564a2f5c0be6fc94b70a682d8312af4d
shwncho <shwncho4@gmail.com>
2026-01-08 11:00:00 +0900

    refactor: MSA 구조로 전환

---
+++ order/src/main/java/com/example/order/infrastructure/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.example.order.infrastructure;
+
+
+import com.example.order.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ point/src/main/java/com/example/point/infrastructure/PointRepository.java
@@ -0,0 +1,9 @@
+package com.example.point.infrastructure;
+
+import com.example.point.domain.Point;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PointRepository extends JpaRepository<Point, Long> {
+
+    Point findByUserId(Long userId);
+}
